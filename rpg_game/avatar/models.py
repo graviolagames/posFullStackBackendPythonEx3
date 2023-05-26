caef40b9143e5723d69289e8cc4c7c957a867943
graviolagames <graviolagames@gmail.com>
2023-05-26 16:54:08 -0300

    Create Avatar models

--- rpg_game/avatar/models.py
+++ rpg_game/avatar/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Class(models.Model):
+    name = models.CharField(max_length=100)
+    strength = models.IntegerField()
+    ability = models.IntegerField()
+    magic = models.IntegerField()
+
+
+class InventoryItems(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    carga = models.IntegerField()
+    damage = models.IntegerField()
+    cure = models.IntegerField()
+
+
+class Avatar(models.Model):
+    name = models.CharField(max_length=100)
+    health = models.IntegerField()
+    experience = models.IntegerField()
+    reputation = models.IntegerField()
+    idClass = models.ForeignKey(Class, on_delete=models.CASCADE)
+    Element = models.CharField(max_length=100)
+    background = models.TextField()
+    idInventory = models.ForeignKey(InventoryItems, on_delete=models.CASCADE)
