8666daddf6623ab82d41a2eada22eebfcca5d1ef
graviolagames <graviolagames@gmail.com>
2023-05-28 09:34:41 -0300

    Fix TemplateDoesNotExist error

--- rpg_game/avatar/views.py
+++ rpg_game/avatar/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import View
+from .models import Avatar
 
 # Create your views here.
+class AvatarView(View):
+    def get(self, request):
+        avatars = Avatar.objects.all()
+        return render(request, 'avatar/avatar_list.html', {'avatars': avatars})
