4e3603b2f8de433f0302569d1004f645a85ba27e
graviolagames <graviolagames@gmail.com>
2023-05-28 11:48:25 -0300

    Introduce AvatarClass Model

--- rpg_game/avatar/admin.py
+++ rpg_game/avatar/admin.py
@@ -2,9 +2,12 @@ from django.contrib.admin import site
 from django.contrib import admin
 from avatar.models import Avatar
 from avatar.models import AvatarClass
+from avatar.models import AvatarElement
+
 
 
 # Register your models here.
 site.register(Avatar)
 site.register(AvatarClass)
+site.register(AvatarElement)
 
--- rpg_game/avatar/models.py
+++ rpg_game/avatar/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 
+#Model for 
 class AvatarClass(models.Model):
     name = models.CharField(max_length=100)
     strength = models.IntegerField()
@@ -8,14 +9,20 @@ class AvatarClass(models.Model):
     magic = models.IntegerField()
     def __str__(self):
         return self.name
-    
+
+class AvatarElement(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+
+
 class Avatar(models.Model):
     name = models.CharField(max_length=100)
     health = models.IntegerField()
     experience = models.IntegerField()
     reputation = models.IntegerField()
     avatarClass = models.ForeignKey(AvatarClass, on_delete=models.CASCADE)
-    Element = models.CharField(max_length=100)
+    avatarElement = models.ForeignKey(AvatarElement, on_delete=models.CASCADE)
     background = models.TextField()
     def __str__(self):
         return self.name
